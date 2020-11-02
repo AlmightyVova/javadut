5c79014c1b7e0be9b542e98296825d4b0f0d0f3f
Vladimir Legno <vovkolimur@ukr.net>
2020-11-02 07:26:58 +0200

    preparing to fix

--- Lab2/2userClass/src/com/company/UserDto.java
+++
@@ -1,7 +0,0 @@
-package com.company;
-
-public class UserDto {
-    public String name;
-    public String age;
-    public String email;
-}
--- Lab2/2userClass/src/com/company/UserMapper.java
+++
@@ -1,6 +0,0 @@
-package com.company;
-
-public class UserMapper {
-    public void mapToUser(UserDto userDto) {}
-    public void mapToDto(User user){}
-}
